807bbaad856c4ce56aec10c95a299c09186a6861
sharmadotexe <rockvikas300@gmail.com>
2025-01-20 21:53:47 +0530

    side bar component completed

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,23 +1,23 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 
 
 
 
 //for viewing all users except us
-export const getUsersForSideBar =  async (req,res) =>{
+export const getUsersForSideBar = async (req, res) => {
     try {
-        const loggedInUserId = req.user._id;
-        const filteredUsers = (await User.find({_id:{$ne: loggedInUserId}})).select("-password");
-
-        res.status(200).json(filteredUsers)
-
+      const loggedInUserId = req.user._id;
+      const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+  
+      res.status(200).json(filteredUsers);
     } catch (error) {
-         console.log("Error in getUsersForSideBar", error.message);
-         res.status(500).json({message: "INTERNAL SERVICE ERROR"});
+      console.error("Error in getUsersForSidebar: ", error.message);
+      res.status(500).json({ error: "Internal server error" });
     }
-};
+  };
 
 
 //for entering chat and viewing messages
