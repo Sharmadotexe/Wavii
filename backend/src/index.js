bf02bb8333f8c36b0f903a433562e57c9e23fb0c
sharmadotexe <rockvikas300@gmail.com>
2025-01-23 13:24:08 +0530

    Changes made for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -45,13 +45,13 @@ app.use("/api/messages", messageRoutes);
 
 
 //prod
-if(process.env.NODE.ENV === "production"){
+if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req,res)=>{
         res.sendFile(path.join(__dirname, "../frontend", "dist","index.html"));
     })
-}
+} 
 
 
 
